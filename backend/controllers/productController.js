22b6f538c60eb6b5f2821f48961ce5c4d0382865
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-06-30 12:37:29 +0530

    Get /  Delete product by Id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,6 +16,20 @@ module.exports.getAllProducts = async (req, res) => {
   res.status(200).json({ success: true, products });
 };
 
+// Get Product by ID
+exports.getProductDetails = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found!",
+    });
+  }
+
+  return res.status(200).json({ success: true, product });
+};
+
 // Update Product -- Admin
 module.exports.updateProduct = async (req, res) => {
   let product = await Product.findById(req.params.id);
@@ -32,5 +46,24 @@ module.exports.updateProduct = async (req, res) => {
     useFindAndModify: false,
   });
 
-  return res.status(200).json({ success: true, product })
+  return res.status(200).json({ success: true, product });
+};
+
+// Delete Product -- Admin
+exports.deleteProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found!",
+    });
+  }
+
+  // await Product.findByIdAndDelete(req.params.id);
+  await product.remove();
+  return res.status(200).json({
+    success: true,
+    message: `${product.name} deleted successfully.`,
+  });
 };
