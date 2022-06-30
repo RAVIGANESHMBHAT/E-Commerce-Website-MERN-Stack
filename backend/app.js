43c4126f2c2c8e6f2a7b9b2816cdfa4af19f1df4
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-06-30 12:22:58 +0530

    Update product

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // Route Imports
-const product = require('./routes/productRoute')
+const product = require("./routes/productRoute");
 
-app.use("/api/v1", product)
+app.use(express.json());
+app.use("/api/v1", product);
 
-module.exports = app
+module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,36 @@
-exports.getAllProducts = (req, res) => {
-res.status(200).json({ message: 'Route working'})
-}
+const Product = require("../models/productModel");
+
+// Create Product -- Admin
+module.exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Get All Products
+module.exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({ success: true, products });
+};
+
+// Update Product -- Admin
+module.exports.updateProduct = async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found!",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  return res.status(200).json({ success: true, product })
+};
