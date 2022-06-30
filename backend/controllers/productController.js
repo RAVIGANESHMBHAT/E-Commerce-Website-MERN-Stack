c38e9b6bbf0c2802b7386b8bf8f90f2866b09469
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-06-30 16:52:13 +0530

    Added search + filter for products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create Product -- Admin
 module.exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -14,7 +15,11 @@ module.exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Products
 module.exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();
+
+  const products = await apiFeature.query;
   res.status(200).json({ success: true, products });
 });
 
