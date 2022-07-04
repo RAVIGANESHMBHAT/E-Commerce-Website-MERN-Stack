a3a3256171b761732afdf9206275ce5b1f153b6d
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-07-04 16:44:52 +0530

    Implemented Search Product feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Footer from "./component/layout/Footer/Footer";
 import Header from "./component/layout/Header/Header.js";
 import Home from "./component/Home/Home.js";
 import ProductDetails from "./component/Product/ProductDetails.js"
+import Products from "./component/Product/Products.js"
+import Search from "./component/Product/Search.js"
 
 function App() {
   useEffect(() => {
@@ -22,6 +24,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/product/:id" element={<ProductDetails />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/products/:keyword" element={<Products />} />
+        <Route exact path="/search" element={<Search />} />
       </Routes>
 
       <Footer />
